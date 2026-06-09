878dee66eb83ff1125afb498c4e18c794b8714de
Jagadeep Kalluri <jagadeep.kalluri29@gmail.com>
2026-06-09 15:14:04 -0500

    Add: design-word-search-data-structure - submission-5

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-5.py
@@ -0,0 +1,40 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        def dfs(i, root):
+            curr = root
+            for j in range(i, len(word)):
+                if word[j] == ".":
+                    for k in curr.children.values():
+                        if dfs(j+1, k):
+                            return True
+                    return False
+                else:
+                    if word[j] not in curr.children:
+                        return False
+                    curr = curr.children[word[j]]
+
+            return curr.endOfWord
+
+
+
+        
+        return dfs(0, self.root)
+            
+        
