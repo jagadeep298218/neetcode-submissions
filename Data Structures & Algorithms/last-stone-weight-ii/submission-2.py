55a42ff19bdf2f21a9d2beb2766d7d2e7509f234
Jagadeep Kalluri <jagadeep.kalluri29@gmail.com>
2026-06-25 15:09:12 -0500

    Add: last-stone-weight-ii - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        Stone = sum(stones)
+        S = (Stone + 1) // 2
+        n = len(stones)
+
+        dp = [[0] * S for _ in range(n + 1)]
+
+        for c in range(S):
+            dp[n][c] = abs(Stone - 2 * c)
+
+        for i in range(n - 1, -1, -1):
+            for c in range(S):
+                skip = dp[i + 1][c]
+                nxt = c + stones[i]
+                if nxt >= S:                     
+                    take = abs(Stone - 2 * nxt)
+                else:
+                    take = dp[i + 1][nxt]
+                dp[i][c] = min(skip, take)
+
+        return dp[0][0]                          
