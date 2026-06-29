98c118982783dc4b0f914076f0941c3e346a76c4
Jagadeep Kalluri <jagadeep.kalluri29@gmail.com>
2026-06-29 18:47:42 -0500

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        course_map = {i: [] for i in range(numCourses)}
+        for c, p in prerequisites:
+            course_map[c].append(p)
+
+        res = []
+        seen = set()    
+        done = set()     
+        def dfs(c):
+            if c in seen:    
+                return False
+            if c in res:     
+                return True
+
+            seen.add(c)
+            for pre in course_map[c]:
+                if not dfs(pre):
+                    return False
+            seen.remove(c)
+            res.append(c)     
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return []
+        return res
